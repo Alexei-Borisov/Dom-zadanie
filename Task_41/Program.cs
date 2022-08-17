db5a92e282117fc40921a31f212a498eb727d24a
alexey <alexey_borisoff@mail.ru>
2022-08-17 20:56:25 +0300

    Task_41

---
+++ Task_41/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 4
+*/
+
+Console.WriteLine("Введите кол-во элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+        count++;
+}
+Console.WriteLine("Кол-во чисел > 0 " + count);
