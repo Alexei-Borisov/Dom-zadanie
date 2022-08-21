09ee7644967c0cc18d6386ce116cb790ffa7ae30
alexey <alexey_borisoff@mail.ru>
2022-08-21 21:26:13 +0300

    Task_47

---
+++ Task_47/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцев: ");
+int m = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[n, m];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        matrix[i, j] = new Random().NextDouble() * 100;
+        Console.Write("{0,6:F2}", matrix[i, j]);
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
